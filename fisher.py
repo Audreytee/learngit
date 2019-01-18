10ed4fc9f0587b5b8ee68a03c3138e8ae502e791
ZTT001 <2944998544@qq.com>
2019-01-18 20:24:19 +0800

    fisherbook

---
+++ fisher.py
@@ -0,0 +1,11 @@
+from app import create_app
+
+# 主线程
+app = create_app()
+
+if __name__ == '__main__':
+    # 生产环境：nginx+uwsigi执行
+
+
+    # threaded:单进程，多线程
+    app.run(debug=app.config['DEBUG'], port=81, threaded=True)
